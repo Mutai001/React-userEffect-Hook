2c6ded27ce92f4d54afce5d7341e131de673659a
Mutai001 <omusekim@gmail.com>
2024-06-25 10:25:25 +0300

    useRef $ useState

---
+++ UseRef &UseState/src/App.tsx
@@ -0,0 +1,16 @@
+import { UseRef } from './hooks/UseRef'
+import { UseState } from './hooks/UseState'
+import './App.css'
+
+function App() {
+
+  
+  return (
+    <>
+
+
+    </>
+  )
+}
+
+export default App
--- src/App.tsx
+++ src/App.tsx
@@ -64,3 +64,6 @@ useEffect(() => {
 }
 
 export default App
+
+
+
